ee506805846b84a7ef814a8fab6714ba6785ca11
Ralf Wigand <ralfwi@microsoft.com>
2020-12-09 13:15:40 +0100

    testformat als Table

--- showevents.ps1
+++ showevents.ps1
@@ -8,16 +8,18 @@ Get-DiagnosticDataCategories |ForEach-Object {
 
 $events = get-content -Path "./events.json" |convertfrom-json 
 
-Get-DiagnosticData -starttime (get-date).AddDays(-1) -RequiredTelemetryOnly |out-gridview  -Title "Events (required)" -PassThru |foreach-object {
+Get-DiagnosticData -starttime (get-date -hour 0 -minute 0 -second 0).AddDays(-1) -endtime (get-date -hour 0 -minute 0 -second 0) -RequiredTelemetryOnly | foreach-object {
+# Get-DiagnosticData -starttime (get-date -hour 0 -minute 0 -second 0).AddDays(-1) -endtime (get-date -hour 0 -minute 0 -second 0) -RequiredTelemetryOnly |out-gridview  -Title "Events (required)" -PassThru |foreach-object {
 
     $payload=$_.payload | ConvertFrom-Json
     write-output "<table>"
-    "Version: {0}" -f $payload.ver 
+    # "Version: {0}" -f $payload.ver 
     "Eventname: {0}" -f $payload.Name
-    "Beschreibung des Events: {0}" -f $events.($payload.Name).description
+    "Beschreibung des Events: {0}" -f $events.($payload.Name).eventdescription
+    "<table>"
 
     foreach ($field in ($payload.data | get-member -MemberType NoteProperty | select-object -ExpandProperty Name)){
-        "  {0} hat den Wert {1} und beschreibt {2}" -f $field,$payload.data.$field,$events.($payload.Name).$field
+        "<tr><td>{0}</td><td>{1}</td><td>{2}</td></tr>" -f $field,$payload.data.$field,$events.($payload.Name).$field
 
     }
     # $data=$payload.data
